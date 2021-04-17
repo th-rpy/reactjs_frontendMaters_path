900336662ef8ba91a3afe4ae24d7bec094c96b52
Thamer Saraei <tsarei@legroupejsv.onmicrosoft.com>
2021-04-17 01:08:56 -0400

    work with npm scripts

--- Course1_Complete Intro to React/01_Day/src/App.js
+++ Course1_Complete Intro to React/01_Day/src/App.js
@@ -1,28 +1,24 @@
-// Create our first React Component 
+// Create our first React Component
 const Pet = (props) => {
-    return React.createElement(
-        'div', 
-        {}, 
-        [
-            React.createElement('h2', {}, props.name),
-            React.createElement('h2', {}, props.animal),
-            React.createElement('h2', {}, props.family)
-        ]
-    )
+  return React.createElement("div", {}, [
+    React.createElement("h2", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.family),
+  ]);
 };
 
 const App = () => {
-     return React.createElement(
-        'div',
-        {id : 'div_app'},
-        React.createElement('h1', {}, 'Adopt Me !'), React.createElement(Pet, {
-            name: 'Luna', animal: 'Dog', family: 'Rex'
-        })
-        )
-    };
+  return React.createElement(
+    "div",
+    { id: "div_app" },
+    React.createElement("h1", {}, "Adopt Me !"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      family: "Rex",
+    })
+  );
+};
 
 // Put our component in the 'root' div element
- ReactDOM.render(
-    React.createElement(App),
-    document.getElementById('root')
-    );
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
